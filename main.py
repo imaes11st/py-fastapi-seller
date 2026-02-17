dcf34cc88d3ede7003fd9e546f6fbec014e20a9e
Juan Esteban M <imaes.st@gmail.com>
2026-02-16 22:44:35 -0500

    using import of zoneinfo and datetime

--- main.py
+++ main.py
@@ -1,7 +1,36 @@
 from fastapi import FastAPI
+from datetime import datetime
+from zoneinfo import ZoneInfo
 
 app = FastAPI()
 
 @app.get("/")
 async def root():
-    return {"message": "Configuracion Fast API"}
+    return {"message": "Configuration Fast API"}
+
+country_timezones = {
+    "NI": "America/Managua",              # Nicaragua
+    "CO": "America/Bogota",               # Colombia
+    "MX": "America/Mexico_City",          # México
+    "AR": "America/Argentina/Buenos_Aires", # Argentina
+    "PE": "America/Lima",                 # Perú
+    "US": "America/New_York",             # Estados Unidos
+    "ES": "Europe/Madrid",                # España
+    "JP": "Asia/Tokyo",                   # Japón
+    "AU": "Australia/Sydney",             # Australia
+    "BR": "America/Sao_Paulo"             # Brasil
+}
+
+@app.get('/time/{iso_code}')
+async def time(iso_code: str):
+    iso = iso_code.upper()
+    timezone_str = country_timezones.get(iso)
+    tz = ZoneInfo(timezone_str) 
+    return { "time": datetime.now(tz) }
+
+
+
+
+
+
+
