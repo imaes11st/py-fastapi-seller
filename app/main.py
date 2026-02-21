118e3ca99ce0a34e4b0fa85641e0e91754a43602
Juan Esteban M <imaes.st@gmail.com>
2026-02-21 00:56:59 -0500

    Middleware log request time

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
-from fastapi import FastAPI
+import time
 
+from fastapi import FastAPI, Request
 from sqlmodel import select
 
 from  db import SessionDep, create_all_tables
@@ -11,6 +12,20 @@ app = FastAPI(lifespan=create_all_tables)
 app.include_router(customers.router)
 app.include_router(transactions.router)
 app.include_router(plans.router) 
+
+
+@app.middleware("http")
+async def log_request_time(request: Request, call_next):
+    start_time = time.time()
+    response = await call_next(request)
+    process_time = time.time() - start_time
+    print(f"Request: {request.url} - Complete in {process_time:.4f} seconds")
+    return response
+
+
+@app.get("/")
+async def root():
+    return {"message": "Welcome to the FastAPI project!"}
 
 
 @app.post('/invoices')
